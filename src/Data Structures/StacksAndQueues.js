49e0e51afb4a40e226ca0977724612891ea0db7e
TylerKonesky <tylerkonesky@hotmail.com>
2020-09-10 20:08:24 -0600

    file

---
+++ src/Data Structures/StacksAndQueues.js
@@ -0,0 +1,3 @@
+// What is a Stack? What do we use them for?
+// Both Stacks and Queues are abstract data sctructures-- Must use a LIFO method, the last 
+// element added to a stack will be the first one removed. 
