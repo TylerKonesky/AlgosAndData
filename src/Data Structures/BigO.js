c54d984e88f8c5709e3d602a4d93109b2143cae9
TylerKonesky <tylerkonesky@hotmail.com>
2020-09-14 18:34:46 -0600

    stacks and queues

--- src/Data Structures/BigO.js
+++ src/Data Structures/BigO.js
@@ -1,3 +1,5 @@
 //                              Insertion       Removal         Searching           Access                        
 // Singly Linked Lists            O(1)         O(1) or O(n)       O(n)               O(n)
 // Doubly Linked Lists            O(1)            O(1)            O(n)               O(n/2) or O(n)
+// Stacks                         O(1)            O(1)            O(n)               O(n)
+// Queues                         O(1)            O(1)            O(n)               O(n)
--- src/Data Structures/StacksAndQueues.js
+++ src/Data Structures/StacksAndQueues.js
@@ -1,3 +1,87 @@
 // What is a Stack? What do we use them for?
 // Both Stacks and Queues are abstract data sctructures-- Must use a LIFO method, the last 
 // element added to a stack will be the first one removed. 
+
+
+// ************ STACK ************
+class Node {
+    constructor(val){
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val){
+        let newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+
+        return ++this.size;
+    }
+
+    pop(){
+        if(!this.first) return null;
+        let temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+
+        return temp.value;
+    }
+}
+
+
+// ************ Queues ************
+
+// What is a queue --->>> FIRST IN FIRST OUT data structure (Think like a queue of orders... or a line in a store...)
+// enqueue and dequeue
+
+class Queue {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+// Adds to the queue
+    enqueue(val){
+        let newNode = new Node(val);
+        if(this.size === 0){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+// Removes from the queue
+    dequeue(){
+        if(!this.first) return null;
+        let temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--
+        return temp.value;
+    }
+
+
+}
+
